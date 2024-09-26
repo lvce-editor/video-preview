7dab79c906a7a50151ce1a146f48956d50151061
Le Vivilet <levivlet@pm.me>
2024-09-26 19:13:31 +0200

    copy files

--- packages/build/src/copy-example-files.js
+++ packages/build/src/copy-example-files.js
@@ -3,29 +3,49 @@ import { tmpdir } from 'os'
 import { dirname, join } from 'path'
 import { execa } from 'execa'
 import { root } from './root.js'
+import { existsSync } from 'fs'
 
 const url = 'https://github.com/mediaelement/mediaelement-files'
-const toCopy = ['big_buck_bunny.mp4', 'big_buck_bunny.webm', 'echo-hereweare.ogv']
+const toRemove = [
+  'AirReview-Landmarks-02-ChasingCorporate.mp3',
+  'big_buck_bunny.jpg',
+  'echo-hereweare.jpg',
+  'echo-hereweare.mp4',
+  'echo-hereweare.webm',
+  'guqin.flv',
+  'mediaelement.srt',
+  'README.md',
+]
 
-const main = async () => {
-  const tmpDir = join(tmpdir(), 'mediaelement-files')
+const downloadRepo = async (tmpDir) => {
+  if (existsSync(tmpDir)) {
+    return
+  }
   await rm(tmpDir, { recursive: true })
   await mkdir(dirname(tmpDir), { recursive: true })
   await execa('git', ['clone', url, tmpDir], { stdio: 'inherit' })
+}
+
+const copyRepoFiles = async (tmpDir) => {
   const outDir = join(root, 'packages', 'sample-files', 'files')
   await mkdir(outDir, { recursive: true })
   await cp(tmpDir, outDir, {
     recursive: true,
-    filter(source, destination) {
-      console.log({ source })
-      for (const extension of toCopy) {
-        if (source.endsWith(extension)) {
-          return true
-        }
-      }
-      return false
-    },
   })
+}
+
+const removeUnusedFiles = async () => {
+  const outDir = join(root, 'packages', 'sample-files', 'files')
+  for (const item of toRemove) {
+    await rm(join(outDir, item))
+  }
+}
+
+const main = async () => {
+  const tmpDir = join(tmpdir(), 'mediaelement-files')
+  await downloadRepo(tmpDir)
+  await copyRepoFiles(tmpDir)
+  await removeUnusedFiles()
 }
 
 main()
