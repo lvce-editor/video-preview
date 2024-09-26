6cb1200fc884b195cfb975619f6aaaf594c59b07
Le Vivilet <levivlet@pm.me>
2024-09-26 18:50:50 +0200

    rename

--- packages/video-preview-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/video-preview-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,13 +1,1 @@
-import * as Create from '../Create/Create.ts'
-import * as GetUrl from '../GetUrl/GetUrl.ts'
-import * as HandlePointerDown from '../HandlePointerDown/HandlePointerDown.ts'
-import * as HandlePointerMove from '../HandlePointerMove/HandlePointerMove.ts'
-import * as HandlePointerUp from '../HandlePointerUp/HandlePointerUp.ts'
-
-export const commandMap = {
-  'videoPreview.getUrl': GetUrl.getUrl,
-  'videoPreview.handlePointerDown': HandlePointerDown.handlePointerDown,
-  'videoPreview.handlePointerUp': HandlePointerUp.handlePointerUp,
-  'videoPreview.handlePointerMove': HandlePointerMove.handlePointerMove,
-  'videoPreview.create': Create.create,
-}
+export const commandMap = {}
--- packages/video-preview-worker/src/parts/GetRemoteUrl/GetRemoteUrl.ts
+++
@@ -1,6 +0,0 @@
-export const getRemoteUrl = (uri: string) => {
-  // TODO maybe instead of hardcoding path,
-  // ask extension api to generate a remote url for a file path
-  // e.g. vscode.getRemoteUrl(uri)
-  return `/remote/${uri}`
-}
