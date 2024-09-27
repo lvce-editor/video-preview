96a2e5948a6a108c12de92cbe5edb10e4885cf2f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-27 18:00:48 +0200

    feature: use builtin api to create remote url (#8)

--- packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
+++ packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
@@ -1,11 +1,9 @@
-import * as VideoPreviewWorker from '../VideoPreviewWorker/VideoPreviewWorker.ts'
-
 export const webViewProvider = {
   id: 'builtin.video-preview',
   async create(webView, uri) {
     // TODO if can use remote uri, use remote uri, else read file
     // @ts-ignore
-    const remoteUrl = await VideoPreviewWorker.invoke('VideoPreview.getUrl', uri)
+    const remoteUrl = await vscode.getRemoteUrl(uri)
     await webView.invoke('initialize', remoteUrl)
     // @ts-ignore
     webViewProvider.webView = webView
