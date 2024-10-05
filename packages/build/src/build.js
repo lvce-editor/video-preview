ce0cc6aee91160b0a3ae4dbd6a5f5bd08ac1dc4a
Le Vivilet <levivlet@pm.me>
2024-10-05 08:27:30 +0200

    build

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -31,20 +31,6 @@ fs.cpSync(join(videoPreviewWorker, 'src'), join(root, 'dist', 'video-preview-wor
   recursive: true,
 })
 
-const workerUrlFilePath = path.join(root, 'dist', 'src', 'parts', 'VideoPreviewWorkerUrl', 'VideoPreviewWorkerUrl.ts')
-await replace({
-  path: workerUrlFilePath,
-  occurrence: 'src/videoPreviewWorkerMain.ts',
-  replacement: 'dist/videoPreviewWorkerMain.js',
-})
-
-const assetDirPath = path.join(root, 'dist', 'src', 'parts', 'AssetDir', 'AssetDir.ts')
-await replace({
-  path: assetDirPath,
-  occurrence: '../../../../',
-  replacement: '../',
-})
-
 await replace({
   path: join(root, 'dist', 'extension.json'),
   occurrence: 'src/videoPreviewMain.ts',
