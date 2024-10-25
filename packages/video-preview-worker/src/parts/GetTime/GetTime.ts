d716818957e7884d1da5e7e96a532e2f8b9d358c
Le Vivilet <levivlet@pm.me>
2024-10-25 20:44:39 +0200

    feature: save and deserialize video time

---
+++ packages/video-preview-worker/src/parts/GetTime/GetTime.ts
@@ -0,0 +1,7 @@
+import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
+
+export const getTime = (id: number): number => {
+  const state = WebViewStates.get(id)
+  const time = state.time
+  return time
+}
