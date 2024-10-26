2c9ea1fb69d3f8e6cbbfdc77ff4b12e6772e3b12
Le Vivilet <levivlet@pm.me>
2024-10-26 12:03:14 +0200

    feature: include video files in static build

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,6 +1,6 @@
 import { replace } from '@lvce-editor/package-extension'
 import { exportStatic } from '@lvce-editor/shared-process'
-import { cp, readFile, writeFile } from 'node:fs/promises'
+import { cp, readdir, readFile, rm, writeFile } from 'node:fs/promises'
 import path, { join } from 'node:path'
 import { root } from './root.js'
 
@@ -40,3 +40,13 @@ const extensionJson = JSON.parse(extensionJsonContent)
 extensionJson.webViews[0].path = `${commitHash}/extensions/${extensionJson.id}/${extensionJson.webViews[0].path}`
 extensionJson.webViews[0].remotePath = `${pathPrefix}/${commitHash}/extensions/${extensionJson.id}`
 await writeFile(webViewsPath, JSON.stringify(extensionJson.webViews, null, 2) + '\n')
+
+
+await rm(join(root, 'dist', commitHash, 'playground'), { recursive: true, force: true })
+await cp(join(root, 'packages', 'sample-files', 'files'), join(root, 'dist', commitHash, 'playground'), {
+  recursive: true,
+})
+
+const dirents = await readdir(join(root, 'dist', commitHash, 'playground'))
+const fileMap = dirents.map((dirent) => `/playground/${dirent}`)
+await writeFile(join(root, 'dist', commitHash, 'config', 'fileMap.json'), JSON.stringify(fileMap, null, 2) + '\n')
