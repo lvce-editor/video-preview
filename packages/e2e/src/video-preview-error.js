f8b1e0bb46016a80db66cbd134fb236d7480e783
Le Vivilet <levivlet@pm.me>
2024-12-22 13:38:01 +0100

    test

--- packages/e2e/src/video-preview-error.js
+++ packages/e2e/src/video-preview-error.js
@@ -1,6 +1,6 @@
 export const name = 'video-preview-error'
 
-export const test = async ({ FileSystem, Main }) => {
+export const test = async ({ FileSystem, Main, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/test.mp4`, `abc`)
@@ -10,4 +10,7 @@ export const test = async ({ FileSystem, Main }) => {
 
   // assert
   // TODO verify an error message is displayed
+  const error = Locator('.Viewlet.Error')
+  await expect(error).toBeVisible()
+  await expect(error).toHaveText('Error: Failed to load video: DEMUXER_ERROR_COULD_NOT_OPEN: FFmpegDemuxer: open context failed')
 }
