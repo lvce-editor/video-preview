cdbbacf44def30b4c651fe7e6bee87a0939b4bdd
Le Vivilet <levivlet@pm.me>
2024-12-22 15:34:16 +0100

    feature: move server into separate file

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,8 +1,8 @@
 import { replace } from '@lvce-editor/package-extension'
-import { exportStatic } from '@lvce-editor/shared-process'
 import { cp, readdir, readFile, rm, writeFile } from 'node:fs/promises'
 import path, { join } from 'node:path'
 import { root } from './root.js'
+import { pathToFileURL } from 'node:url'
 
 await import('./build.js')
 
@@ -10,6 +10,10 @@ await cp(path.join(root, 'dist'), path.join(root, 'dist2'), {
   recursive: true,
   force: true,
 })
+
+const sharedProcessPath = join(root, 'packages', 'server', 'node_modules', '@lvce-editor', 'shared-process', 'index.js')
+const sharedProcessUri = pathToFileURL(sharedProcessPath).toString()
+const { exportStatic } = await import(sharedProcessUri)
 
 const { commitHash } = await exportStatic({
   extensionPath: 'packages/extension',
@@ -40,7 +44,6 @@ const extensionJson = JSON.parse(extensionJsonContent)
 extensionJson.webViews[0].path = `${commitHash}/extensions/${extensionJson.id}/${extensionJson.webViews[0].path}`
 extensionJson.webViews[0].remotePath = `${pathPrefix}/${commitHash}/extensions/${extensionJson.id}`
 await writeFile(webViewsPath, JSON.stringify(extensionJson.webViews, null, 2) + '\n')
-
 
 await rm(join(root, 'dist', commitHash, 'playground'), { recursive: true, force: true })
 await cp(join(root, 'packages', 'sample-files', 'files'), join(root, 'dist', commitHash, 'playground'), {
