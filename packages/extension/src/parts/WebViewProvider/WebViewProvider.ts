4bd22ecdd0f14f5950d06d9fea8944ec96539138
Le Vivilet <levivlet@pm.me>
2024-12-22 13:41:14 +0100

    add todo

--- packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
+++ packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
@@ -4,6 +4,9 @@ import * as WebViewId from '../WebViewId/WebViewId.ts'
 
 const id = 1
 
+// TODO move all of this to video worker.
+// then the video worker only asks for remoteUrl, which is provided by vscode.getRemoteUrl
+
 export const webViewProvider = {
   id: WebViewId.webViewId,
   async create(webView, uri, savedState) {
