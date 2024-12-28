6dd2cc7e9db62e1ee201fe098903a27a663c9b12
Le Vivilet <levivlet@pm.me>
2024-12-28 17:45:47 +0100

    feature: try to move more webview code to worker

--- packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
+++ packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
@@ -1,4 +1,3 @@
-import { VideoLoadError } from '../VideoLoadError/VideoLoadError.ts'
 import * as VideoPreviewWorker from '../VideoPreviewWorker/VideoPreviewWorker.ts'
 import * as WebViewId from '../WebViewId/WebViewId.ts'
 
@@ -11,26 +10,27 @@ export const webViewProvider = {
   id: WebViewId.webViewId,
   async create(webView, uri, savedState) {
     // TODO if can use remote uri, use remote uri, else read file
-    await VideoPreviewWorker.invoke('VideoPreview.create', id)
-    await VideoPreviewWorker.invoke('VideoPreview.setSavedState', id, savedState)
-    // @ts-ignore
-    // @ts-ignore
-    const remoteUrl = await vscode.getRemoteUrl(uri, {
-      webViewId: WebViewId.webViewId,
-    })
-    const time = await VideoPreviewWorker.invoke('VideoPreview.getTime', id)
-    const event = await webView.invoke('initialize', remoteUrl, time)
-    if (event.type === 'error') {
-      throw new VideoLoadError(event)
-    }
+    // await VideoPreviewWorker.invoke('VideoPreview.create', id)
+    // await VideoPreviewWorker.invoke('VideoPreview.setSavedState', id, savedState)
+    // // @ts-ignore
+    // // @ts-ignore
+    // const remoteUrl = await vscode.getRemoteUrl(uri, {
+    //   webViewId: WebViewId.webViewId,
+    // })
+    // const time = await VideoPreviewWorker.invoke('VideoPreview.getTime', id)
+    // const event = await webView.invoke('initialize', remoteUrl, time)
+    // if (event.type === 'error') {
+    //   throw new VideoLoadError(event)
+    // }
 
     // @ts-ignore
     webViewProvider.webView = webView
   },
   async open(uri, webView) {},
   async saveState() {
-    const state = await VideoPreviewWorker.invoke('VideoPreview.saveState', id)
-    return state
+    // const state = await VideoPreviewWorker.invoke('VideoPreview.saveState', id)
+    // return state
+    return {}
   },
   commands: {
     handleError(code, message) {
--- packages/video-preview-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/video-preview-worker/src/parts/CommandMap/CommandMap.ts
@@ -3,13 +3,47 @@ import * as GetUrl from '../GetUrl/GetUrl.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
 import * as SetSavedState from '../SetSavedState/SetSavedState.ts'
 import * as GetTime from '../GetTime/GetTime.ts'
-import * as SetTime from '../SetTime/SetTime.ts'
+import * as Rpc from '../Rpc/Rpc.ts'
+
+// export const commandMap = {
+// 'VideoPreview.create': Create.create,
+// 'VideoPreview.getTime': GetTime.getTime,
+// 'VideoPreview.getUrl': GetUrl.getUrl,
+// 'VideoPreview.saveState': SaveState.saveState,
+// 'VideoPreview.setSavedState': SetSavedState.setSavedState,
+// 'VideoPreview.setTime': SetTime.setTime,
+// }
+
+const id = 1
+
+const ports = Object.create(null)
+
+const create = async ({ id, savedState, webViewId, uri }) => {
+  console.log('webview create')
+  Create.create(id)
+  SetSavedState.setSavedState(id, savedState)
+  const remoteUrl = await Rpc.invoke('WebView.getRemoteUrl', {
+    uri,
+    webViewId,
+  })
+
+  console.log({ remoteUrl })
+  const time = GetTime.getTime(id)
+  const port = ports[id]
+  const event = await port.invoke('initialize', remoteUrl, time)
+  if (event.type === 'error') {
+    // TODO
+    // throw new VideoLoadError(event)
+  }
+
+  return {}
+}
+
+const setPort = (id, port) => {
+  ports[id] = port
+}
 
 export const commandMap = {
-  'VideoPreview.create': Create.create,
-  'VideoPreview.getTime': GetTime.getTime,
-  'VideoPreview.getUrl': GetUrl.getUrl,
-  'VideoPreview.saveState': SaveState.saveState,
-  'VideoPreview.setSavedState': SetSavedState.setSavedState,
-  'VideoPreview.setTime': SetTime.setTime,
+  'WebView.create': create,
+  'WebView.setPort': setPort,
 }
