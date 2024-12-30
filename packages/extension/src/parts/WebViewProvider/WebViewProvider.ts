7071cf945646901b1ea5345c0a1dcc883fe49222
Le Vivilet <levivlet@pm.me>
2024-12-30 19:28:40 +0100

    remove unused code

--- packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
+++ packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
@@ -1,17 +1,1 @@
-import * as VideoPreviewWorker from '../VideoPreviewWorker/VideoPreviewWorker.ts'
-import * as WebViewId from '../WebViewId/WebViewId.ts'
-
-const id = 1
-
-// TODO move all of this to video worker.
-// then the video worker only asks for remoteUrl, which is provided by vscode.getRemoteUrl
-
-export const webViewProvider = {
-  id: WebViewId.webViewId,
-  async create(webView, uri, savedState) {},
-  async open(uri, webView) {},
-  async saveState() {
-    const state = await VideoPreviewWorker.invoke('VideoPreview.saveState', id)
-    return state
-  },
-}
+export {}
