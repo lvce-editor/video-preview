f15d21df5f01bc9691080f8c05ac5c5e622b0acd
Le Vivilet <levivlet@pm.me>
2024-12-30 19:18:30 +0100

    remove unused code

--- packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
+++ packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
@@ -8,49 +8,10 @@ const id = 1
 
 export const webViewProvider = {
   id: WebViewId.webViewId,
-  async create(webView, uri, savedState) {
-    // TODO if can use remote uri, use remote uri, else read file
-    // await VideoPreviewWorker.invoke('VideoPreview.create', id)
-    // await VideoPreviewWorker.invoke('VideoPreview.setSavedState', id, savedState)
-    // // @ts-ignore
-    // // @ts-ignore
-    // const remoteUrl = await vscode.getRemoteUrl(uri, {
-    //   webViewId: WebViewId.webViewId,
-    // })
-    // const time = await VideoPreviewWorker.invoke('VideoPreview.getTime', id)
-    // const event = await webView.invoke('initialize', remoteUrl, time)
-    // if (event.type === 'error') {
-    //   throw new VideoLoadError(event)
-    // }
-
-    // @ts-ignore
-    webViewProvider.webView = webView
-  },
+  async create(webView, uri, savedState) {},
   async open(uri, webView) {},
   async saveState() {
-    // const state = await VideoPreviewWorker.invoke('VideoPreview.saveState', id)
-    // return state
-    return {}
-  },
-  commands: {
-    handleError(code, message) {
-      // TODO move this to worker
-      // TODO improve error message for file not found error
-      const errorMessage = `Error: Video Failed to load: ${message}`
-      // @ts-ignore
-      webViewProvider.webView.invoke('setError', errorMessage)
-    },
-    handleLoad() {
-      // console.log('video loaded')
-    },
-    async handleTimeUpdate(currentTime) {
-      await VideoPreviewWorker.invoke('VideoPreview.setTime', id, currentTime)
-    },
-    pageHide() {
-      console.log('page hide (worker)')
-    },
-    async beforeUnload() {
-      console.log('before unload (worker)')
-    },
+    const state = await VideoPreviewWorker.invoke('VideoPreview.saveState', id)
+    return state
   },
 }
--- packages/video-preview-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/video-preview-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
 import * as Create from '../Create/Create.ts'
 import * as SetTime from '../SetTime/SetTime.ts'
+import * as SaveState from '../SaveState/SaveState.ts'
 
 export const commandMap = {
   'WebView.create': Create.create,
   handleTimeUpdate: SetTime.setTime,
+  'WebView.saveState': SaveState.saveState,
 }
