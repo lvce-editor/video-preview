d3e99c66590a40598312954095a5c32d7c79c776
Le Vivilet <levivilet@pm.me>
2025-12-16 11:44:00 +0100

    feature: enable eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,15 @@
 import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [...config.default, ...actions.default]
+export default [
+  ...config.default,
+  ...actions.default,
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      'jest/no-restricted-jest-methods': 'off',
+      'github-actions/ci-versions': 'off',
+    },
+  },
+]
