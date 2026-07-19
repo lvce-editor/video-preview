59177dd7c9b3fb0b6b469b45892135934a01f062
Le Vivilet <levivilet@pm.me>
2026-07-19 17:14:26 +0000

    feature: enable recommended virtual dom lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
+  ...config.recommendedVirtualDom,
   ...actions.default,
   {
     rules: {
