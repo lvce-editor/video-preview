63873747e7d05962ac99d8dfbf96b0ec948b7a67
Le Vivilet <levivilet@pm.me>
2026-08-01 16:39:55 -0700

    feature: update eslint dependencies (#128)

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
-import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedVirtualDom,
-  ...actions.default,
+  ...config.recommendedActions,
   {
     rules: {
       '@typescript-eslint/explicit-function-return-type': 'off',
@@ -26,4 +26,4 @@ export default [
       'unicorn/no-global-object-property-assignment': 'off',
     },
   },
-]
+])
