27f4d3489ac07ac1c2e0519d8b5753be99f455dd
Le Vivilet <levivilet@pm.me>
2026-08-03 14:49:40 -0700

    feature: add 20 video preview e2e tests (#131)
    * feature: add video preview e2e coverage
    
    * fix: avoid ogv e2e navigation timeout

---
+++ packages/e2e/src/video-preview-muted-disabled.ts
@@ -0,0 +1,14 @@
+const remotePrefix = '/remote'
+const videoUri = new URL('../../sample-files/files/big_buck_bunny.mp4', import.meta.url).pathname.slice(remotePrefix.length)
+const fileUri = `file://${videoUri}`
+
+export const name = 'video-preview-muted-disabled'
+
+export const test = async ({ expect, Locator, Main }) => {
+  // act
+  await Main.openUri(fileUri)
+
+  // assert
+  const element = Locator('.VideoElement')
+  await expect(element).toHaveJSProperty('muted', false)
+}
---
+++ packages/e2e/src/video-preview-root-overflow.ts
@@ -0,0 +1,14 @@
+const remotePrefix = '/remote'
+const videoUri = new URL('../../sample-files/files/big_buck_bunny.mp4', import.meta.url).pathname.slice(remotePrefix.length)
+const fileUri = `file://${videoUri}`
+
+export const name = 'video-preview-root-overflow'
+
+export const test = async ({ expect, Locator, Main }) => {
+  // act
+  await Main.openUri(fileUri)
+
+  // assert
+  const element = Locator('.VideoPreview')
+  await expect(element).toHaveCSS('overflow', 'hidden')
+}
