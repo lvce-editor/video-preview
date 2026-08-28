7059c59879066b4dc2dd9fe3eb7138bfe79d8fdf
Le Vivilet <levivilet@pm.me>
2026-08-28 21:56:04 +0000

    feature: split video preview render functions (#146)

--- packages/extension/src/parts/RenderVideoPreview/RenderVideoPreview.ts
+++ packages/extension/src/parts/RenderVideoPreview/RenderVideoPreview.ts
@@ -1,71 +1,11 @@
-import { text, VirtualDomElements, type VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-
-export interface VideoPreviewRenderState {
-  readonly errorMessage: string
-  readonly mediaType: 'audio' | 'video'
-  readonly url: string
-}
+import { VirtualDomElements, type VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { VideoPreviewRenderState } from '../VideoPreviewRenderState/VideoPreviewRenderState.ts'
+import { getChildDom } from '../GetChildDom/GetChildDom.ts'
 
 const parentNode: VirtualDomNode = {
   childCount: 1,
   className: 'VideoPreview',
   type: VirtualDomElements.Div,
-}
-
-const errorParentNode: VirtualDomNode = {
-  childCount: 1,
-  className: 'VideoPreviewError',
-  type: VirtualDomElements.Div,
-}
-
-const videoParentNode: VirtualDomNode = {
-  childCount: 1,
-  className: 'VideoContent',
-  type: VirtualDomElements.Div,
-}
-
-const getErrorVirtualDom = (message: string): readonly VirtualDomNode[] => {
-  return [errorParentNode, text(message)]
-}
-
-const getVideoVirtualDom = (url: string): readonly VirtualDomNode[] => {
-  return [
-    videoParentNode,
-    {
-      childCount: 0,
-      className: 'VideoElement',
-      controls: true,
-      onError: DomEventListenerFunctions.HandleVideoError,
-      src: url,
-      type: VirtualDomElements.Video,
-    },
-  ]
-}
-
-const getAudioVirtualDom = (url: string): readonly VirtualDomNode[] => {
-  return [
-    videoParentNode,
-    {
-      childCount: 0,
-      className: 'AudioElement',
-      controls: true,
-      onError: DomEventListenerFunctions.HandleAudioError,
-      src: url,
-      type: VirtualDomElements.Audio,
-    },
-  ]
-}
-
-const getChildDom = (state: Readonly<VideoPreviewRenderState>): readonly VirtualDomNode[] => {
-  const { errorMessage, mediaType, url } = state
-  if (errorMessage) {
-    return getErrorVirtualDom(errorMessage)
-  }
-  if (mediaType === 'audio') {
-    return getAudioVirtualDom(url)
-  }
-  return getVideoVirtualDom(url)
 }
 
 export const render = (state: Readonly<VideoPreviewRenderState>): readonly VirtualDomNode[] => {
